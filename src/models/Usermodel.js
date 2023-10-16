317df0f0c59c63192eeabf69cdf92c98eb7cd07a
SAMUEL <cg3025519@LAB204M35.IFSP>
2023-10-16 15:49:53 -0300

    commentario db

--- src/models/Usermodel.js
+++ src/models/Usermodel.js
@@ -2,18 +2,18 @@ import db from '../database/db.js'
 
 
 const GetUserId = async (id) => {
-    return await db.query("SELECT Nome,Img,Senha FROM users WHERE id =?", [id]
+    return await db.query("SELECT Nome,email,Img,Senha FROM users WHERE id =?", [id]
     )
 }
 
 const UserUpdate = async (user) => {
-    const { id, Nome , Img , Senha } = user
-    return await db.query("UPDATE users SET Nome=?, Img =?,Senha =? WHERE id = ?", [Nome,Img, Senha, id])
+    const { id, Nome, email , Img , Senha } = user
+    return await db.query("UPDATE users SET Nome=?, email=?, Img =?,Senha =? WHERE id = ?", [Nome,email,Img, Senha, id])
 }
 
-const Useradd = async (Nome,Img,Senha) => {
+const Useradd = async (Nome,email,Img,Senha) => {
 
-    return await db.query("INSERT INTO users (Nome,Img,Senha) VALUES (?,?,?) ",[Nome,Img,Senha])
+    return await db.query("INSERT INTO users (Nome,email,Img,Senha) VALUES (?,?,?,?) ",[Nome,email,Img,Senha])
 }
 
 const Del = async (id) => {
